3a4976ffbd68fcf240d902832bcf2489efc5c98e
justayar <cayar@ku.edu.tr>
2017-04-26 00:11:47 +0300

    below zero check

--- SumOfTheFirstNTerms/src/NthSeries.java
+++ SumOfTheFirstNTerms/src/NthSeries.java
@@ -3,10 +3,13 @@ import java.text.DecimalFormat;
 public class NthSeries {
 	public static String seriesSum(int n) {
 	    float sum = 0;
-			for(int i=0;i<n;i++){
-				 double d = (1 / (100.0 * (3*i+1)))*100.0;		       
-			   sum+=d;		        
-			}
-			return String.format("%.2f", sum);
+	    if(n < 0){
+	    	return "";
+	    }
+		for(int i=0;i<n;i++){
+			 double d = (1 / (100.0 * (3*i+1)))*100.0;		       
+			 sum+=d;		        
+		}
+		return String.format("%.2f", sum);
 	}
 }
